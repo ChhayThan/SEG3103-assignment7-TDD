4496adc359306f1101cd03c7c1b3f2aa2749fd1e
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-07-09 02:31:16 -0400

    Add TDD cycles

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,3 +1,139 @@
+// cycle 1
+// function passwordValidator(password) {
+// if (password.length <= 8) {
+//   return "Weak";
+// }
+// }
+
+// cycle 2
+// function passwordValidator(password) {
+//   if (password.length <= 8) {
+//     return "Weak";
+//   }
+
+//   if (/[A-Z]/.test(password) && /[0-9]/.test(password)) {
+//     return "Medium";
+//   } else {
+//     return "Weak";
+//   }
+// }
+
+// cycle 2 refractor
+// function passwordValidator(password) {
+//   if (password.length <= 8) {
+//     return "Weak";
+//   }
+
+//   const hasUpperCase = /[A-Z]/.test(password);
+//   const hasNumber = /[0-9]/.test(password);
+
+//   if (hasUpperCase && hasNumber) {
+//     return "Medium";
+//   }
+//   return "Weak";
+// }
+
+// cycle 3
+// function passwordValidator(password) {
+//   if (password.length <= 8) {
+//     return "Weak";
+//   }
+
+//   const hasUpperCase = /[A-Z]/.test(password);
+//   const hasNumber = /[0-9]/.test(password);
+//   const hasSpecialChar = /[!@#$%^&*]/.test(password);
+
+//   if (
+//     (hasUpperCase && hasNumber) ||
+//     (hasUpperCase && hasSpecialChar) ||
+//     (hasNumber && hasSpecialChar)
+//   ) {
+//     return "Medium";
+//   }
+
+//   return "Weak";
+// }
+
+// cycle 4
+// function passwordValidator(password) {
+//   if (password.length <= 8) {
+//     return "Weak";
+//   }
+
+//   const hasUpperCase = /[A-Z]/.test(password);
+//   const hasNumber = /[0-9]/.test(password);
+//   const hasSpecialChar = /[!@#$%^&*]/.test(password);
+
+//   if (hasUpperCase && hasNumber && hasSpecialChar) {
+//     return "Strong";
+//   }
+
+//   if (
+//     (hasUpperCase && hasNumber) ||
+//     (hasUpperCase && hasSpecialChar) ||
+//     (hasNumber && hasSpecialChar)
+//   ) {
+//     return "Medium";
+//   }
+
+//   return "Weak";
+// }
+
+// cycle 4 refractor
+// function passwordValidator(password) {
+//   if (password.length <= 8) {
+//     return "Weak";
+//   }
+
+//   const hasUpperCase = /[A-Z]/.test(password);
+//   const hasNumber = /[0-9]/.test(password);
+//   const hasSpecialChar = /[!@#$%^&*]/.test(password);
+
+//   let score = 0;
+//   if (hasUpperCase) score++;
+//   if (hasNumber) score++;
+//   if (hasSpecialChar) score++;
+
+//   if (score <= 1) {
+//     return "Weak";
+//   }
+//   if (score === 2) {
+//     return "Medium";
+//   }
+//   if (score === 3) {
+//     return "Strong";
+//   }
+// }
+
+// cycle 5
+// function passwordValidator(password) {
+//   if (password === null || password === undefined) {
+//     throw new Error("Password cannot be null or undefined");
+//   }
+//   if (password.length <= 8) {
+//     return "Weak";
+//   }
+
+//   const hasUpperCase = /[A-Z]/.test(password);
+//   const hasNumber = /[0-9]/.test(password);
+//   const hasSpecialChar = /[!@#$%^&*]/.test(password);
+
+//   let score = 0;
+//   if (hasUpperCase) score++;
+//   if (hasNumber) score++;
+//   if (hasSpecialChar) score++;
+
+//   if (score <= 1) {
+//     return "Weak";
+//   }
+//   if (score === 2) {
+//     return "Medium";
+//   }
+//   if (score === 3) {
+//     return "Strong";
+//   }
+// }
+
 /**
  * Evaluates the strength of a password based on a set of criteria.
  *
--- passwordValidator.test.js
+++ passwordValidator.test.js
@@ -6,18 +6,21 @@ test('should return "Weak" for passwords with 8 or less characters', () => {
 });
 
 // Cycle 2: Test for long password with only one property
-test('should return "Weak" for long passwords with only one other property (e.g., one uppercase letter)', () => {
+test('should return "Weak" for long passwords with only one other property (e.g., one uppercase letter) and return "Medium" for long passwords with two other property', () => {
   expect(passwordValidator("Justupper")).toBe("Weak");
+  expect(passwordValidator("UpperWith1")).toBe("Medium");
 });
 
 // Cycle 3: Test for "Medium" strength
 test('should return "Medium" for long passwords with two other properties (e.g., uppercase and number)', () => {
-  expect(passwordValidator("Justupper1")).toBe("Medium");
+  expect(passwordValidator("UpperWith1")).toBe("Medium");
+  expect(passwordValidator("UpperWith@")).toBe("Medium");
+  expect(passwordValidator("lowerwith@1")).toBe("Medium");
 });
 
 // Cycle 4: Test for "Strong" strength
 test('should return "Strong" for passwords that meet all criteria', () => {
-  expect(passwordValidator("Justupper1!")).toBe("Strong");
+  expect(passwordValidator("StrongPassword@123")).toBe("Strong");
 });
 
 // Cycle 5: Test for edge cases
